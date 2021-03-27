491385b217c882c22e269ca60c5fee390a5c11b2
Ajay <ajay.summer2@gmail.com>
2021-03-27 18:13:17 +0000

    Slowly move invaders left/right and towards bottom

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const grid = document.querySelector('.grid')
 const resultsDisplay = document.querySelector('.results')
 let currentShooterIndex = 202
 let width = 15
-
+let direction = 1
+let goingRight = true
 let aliensRemoved = []
 
 for (let i = 0; i < 225; i++) {
@@ -28,6 +29,41 @@ function draw() {
 }
 
 draw()
+
+function remove() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+      squares[alienInvaders[i]].classList.remove('invader')
+    }
+  }
+
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0
+    const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width -1
+    remove()
+
+    if (rightEdge && goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += width +1
+        direction = -1
+        goingRight = false
+        }
+    }
+
+    if(leftEdge && !goingRight) {
+        for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += width -1
+        direction = 1
+        goingRight = true
+        }
+    }
+
+    for (let i = 0; i < alienInvaders.length; i++) {
+        alienInvaders[i] += direction
+    }
+
+    draw()
+}
+setInterval(moveInvaders, 600)
 
 function moveShooter(event) {
     squares[currentShooterIndex].classList.remove('shooter')
